a0a33a5cb02e3780aeb8884f6a748f14fe0e1e5a
Franco <francoavila.utn@gmail.com>
2024-11-09 19:31:00 -0300

    styles and structure

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  min-width: 100%;
-  min-height: 100%;
+  /* margin-top: 60px; */
+  min-width: 100vw;
+  min-height: 100vh;
 }
 </style>
--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -8,7 +8,7 @@
                 <small v-if="isUserValid === false">El usuario debe ser alfanumérico y contener al menos 5 caracteres.</small>
             </div>
             <div v-if="isUserValid">
-                <ButtonComponent @click="handleLogin" btn-class="btn btn-custom btn-lg" text="Ingresar"/>
+                <ButtonComponent @click="handleLogin" id="btn-custom" btn-class="btn btn-lg" text="Ingresar"/>
             </div>
             <div v-else>
                 <ButtonComponent btn-class="btn btn-secondary btn-lg" text="Ingresar" disabled/>
@@ -56,47 +56,5 @@ export default{
 </script>
 
 <style scoped>
-.login{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 40vw;
-    height: 60vh;
-    gap: 1em;
-    border-radius: 15px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-    animation: fadeInUp 2s ease-out;
-}
-
-@keyframes fadeInUp {
-  0% {
-    opacity: 0;
-    transform: translateY(30px); /* Empieza desde abajo */
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0); /* Llega a su posición final */
-  }
-}
-
-#login-box{
-    min-width: 100vw;
-    min-height: 90vh;
-    background-image: url(../assets/undraw_login_re_4vu2.svg), url(../assets/undraw_floating_re_xtcj.svg) ;
-    background-repeat: no-repeat;
-    background-position: bottom right, top left;
-    background-size: auto, 30% 40%;
-    animation: fadeInUp 1s ease-out;
-}
-
-.btn-custom{
-    background-color: #6c63ff;
-    color: white;
-}
-
-.btn-custom:hover {
-  background-color: #5753e3;
-  color: white;
-}
+@import './LoginView.css';
 </style>
